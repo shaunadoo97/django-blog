d05463a17ea875f9ecd50976da9d6f5ae29c3b03
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-05-14 15:58:57 +0000

    debugging errors

--- blog/models.py
+++ blog/models.py
@@ -8,9 +8,37 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, 
+        on_delete=models.CASCADE, 
+        related_name="blog_posts"
     )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+    updated_on = models.DateField(auto_now=True)
+    field_2 = models.IntegerField(default=42)
+    field_3 = models.CharField(null=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, 
+        related_name="comments"
+        )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        ordering = ["created_on"]
+    def __str__(self):
+        return f"{self.body} by {self.author}"
