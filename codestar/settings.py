a07ee8c252f1558d25d0f912a58cc6dddbc58a8e
Shauna Hathaway McHugh <shaunadoo97@gmail.com>
2024-06-03 15:55:37 +0000

    Updating changes

--- codestar/settings.py
+++ codestar/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -28,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-t1--z*2d54br-e#*jbc4by6vjz*-^cdk5zr-#2_cqjo6q&4i&='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-shaunadoo97-djangoblog-jr9v4xpaxex.ws.codeinstitute-ide.net', '.herokuapp.com' ]
 
@@ -109,6 +110,10 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 CSRF_TRUSTED_ORIGINS = [
